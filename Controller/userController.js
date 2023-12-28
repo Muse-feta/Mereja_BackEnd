53df4b339061194432e8fafd29e4152f5aaeab3f
Muse-Feta <fetamuse@gmail.com>
2023-12-27 18:39:49 -0800

    adding reset password functionility

--- Controller/userController.js
+++ Controller/userController.js
@@ -10,6 +10,10 @@ const checkController = (req, res) => {
     .status(200)
     .json({ status: true, message: "user succesfully checked", email });
 };
+
+// ******************* register controller starts here ******************** //
+
+
 const registerController = async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
 
@@ -18,6 +22,17 @@ const registerController = async (req, res) => {
       .status(500)
       .json({ status: false, message: "please provide all required fields" });
   }
+  
+  if(password.length < 8){
+    return res
+      .status(500)
+      .json({
+        status: false,
+        message: "password must be at lest 8 charachter",
+      });
+  }
+
+  
 
   const hashPassword = await bcrypt.hash(password, 10);
 
@@ -48,6 +63,8 @@ const registerController = async (req, res) => {
     res.status(500).json({ error });
   }
 };
+
+// ********************** Login controller starts here ***************** //
 
 const loginController = async (req, res) => {
   const { email, password } = req.body;
@@ -89,6 +106,8 @@ const loginController = async (req, res) => {
   }
 };
 
+// ******************** forgot password controller starts here ************ //
+
 const forgotController = async (req, res) => {
   try {
     const { email } = req.body;
@@ -97,11 +116,17 @@ const forgotController = async (req, res) => {
       `SELECT * FROM user WHERE email='${email}'`
     );
 
-    const user = response[0][0]
+    const user = response[0][0];
     // console.log(user)
 
+    if (!email) {
+      return res
+        .status(401)
+        .json({ message: "Please Provide all required fields" });
+    }
+
     if (!user) {
-      return res.status(401).json({ message: "User not existed" });
+      return res.status(401).json({ message: "Email not existed" });
     }
 
     const token = jwt.sign({ email: user.email }, process.env.SECRET_KEY, {
@@ -122,7 +147,7 @@ const forgotController = async (req, res) => {
       from: process.env.EMAIL,
       to: `${user.email}`,
       subject: "Reset Your Password",
-      text: `https://muse-mereja-sample-project.netlify.app/reset-password/${user.email}/${token}`,
+      text: `http://localhost:3000/reset/${token}`,
     };
 
     transporter.sendMail(mailOptions, function (error, info) {
@@ -142,9 +167,47 @@ const forgotController = async (req, res) => {
 };
 
 
+// ********************** reset password controller starts here ******** //
+
+const resetController = async (req, res) => {
+  const { token } = req.params;
+  const {password} = req.body;
+  // console.log(token)
+ 
+  if (!password) {
+    return res
+      .status(500)
+      .json({ status: false, message: "please provide all required fields" });
+  }
+  if(password.length < 8) {
+    return res
+      .status(500)
+      .json({
+        status: false,
+        message: "password must be at lest 8 charachter",
+      });
+  }
+  const {email} = jwt.verify(token, process.env.SECRET_KEY)
+  if(!email){
+    return res.status(403).json({message: 'invalid token'})
+  }
+  const hashPassword = await bcrypt.hash(password, 10)
+  try {
+    await connection.query(`UPDATE user SET password = ? WHERE email = ?`, [hashPassword, email])
+    res.status(200).json({status: true, message: 'your password succesfully reseted'})
+  } catch (error) {
+    res.status(500).json(error.message)
+  }
+
+
+
+  
+};
+
 module.exports = {
   checkController,
   registerController,
   loginController,
   forgotController,
+  resetController,
 };
--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -5,6 +5,7 @@ const {
   registerController,
   loginController,
   forgotController,
+  resetController,
 } = require("../Controller/userController");
 const authMiddleWare = require("../Middleware/authMiddleWare");
 
@@ -12,5 +13,6 @@ router.get("/check", authMiddleWare, checkController);
 router.post('/register', registerController)
 router.post('/login', loginController)
 router.post('/forgot-password', forgotController)
+router.post('/reset-password/:token', resetController)
 
 module.exports =  router 
